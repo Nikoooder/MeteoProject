699c1bf770374d73dbef8c9c17f39fdc6b56d095
stergel <katsudounokoto@gmail.com>
2026-07-11 12:04:25 +0000

    Location.cs was added, project was built...succesfully I hope

---
+++ EcoMonitor.Api/Models/Location.cs
@@ -0,0 +1,8 @@
+namespace EcoMonitor.Api.Models;
+
+public class Location{
+	public int Id{get;set;}
+	public string Name{get;set;}
+	public double Latitude{get;set;}
+	public double Longitude{get;set;}
+}
