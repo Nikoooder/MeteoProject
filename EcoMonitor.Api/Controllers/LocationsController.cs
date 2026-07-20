f5adfdc2fc2eda3abbf344caefbfcb59fd0d9596
stergel <katsudounokoto@gmail.com>
2026-07-20 12:28:59 +0000

    API testings branch base

---
+++ EcoMonitor.Api/Controllers/LocationsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EcoMonitor.Api.Models;
+using EcoMonitor.Api.Data;
+
+namespace EcoMonitor.Api.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+
+
+public class LocationsController : ControllerBase{
+
+}
