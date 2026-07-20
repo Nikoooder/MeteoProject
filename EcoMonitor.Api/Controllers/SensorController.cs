5edb14c76f6cac55bb11ac02add96b353c093bec
Tejin <zadrotvbs@gmail.com>
2026-07-20 21:53:31 +0600

    изменение контроллера

--- EcoMonitor.Api/Controllers/SensorController.cs
+++ EcoMonitor.Api/Controllers/SensorController.cs
@@ -2,15 +2,22 @@ using EcoMonitor.Api.Data;
 using EcoMonitor.Api.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-
+namespace EcoMonitor.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class SensorController(){
+public class SensorController: ControllerBase{
     private readonly AppDbContext _context;
+    public SensorController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
     public async Task<List<Sensor>> GetAllAsync()
         {
             return await _context.Sensors.ToListAsync();
         }
+    
 }
 
--- EcoMonitor.Api/Models/Sensor.cs
+++ EcoMonitor.Api/Models/Sensor.cs
@@ -4,5 +4,5 @@ public class Sensor{
 	public int Id{get;set;}
 	public string Name{get;set;}
 	public int LocationId{get;set;}
-	public Location Locations{get;set;}
+	//public Location Locations{get;set;}
 }
