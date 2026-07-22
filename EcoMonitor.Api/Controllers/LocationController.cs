392aec13264936d5a0571d43922896efda8d5883
Tejin <zadrotvbs@gmail.com>
2026-07-23 00:50:02 +0600

    working controllers for models

---
+++ EcoMonitor.Api/Controllers/LocationController.cs
@@ -0,0 +1,62 @@
+using EcoMonitor.Api.Data;
+using EcoMonitor.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+namespace EcoMonitor.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LocationController:ControllerBase{
+    private readonly AppDbContext _context;
+    public LocationController(AppDbContext context){
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<List<Location>> GetAllAsync(){
+        return await _context.Locations.ToListAsync();
+    }
+
+    [HttpGet("{id}", Name = "GetLocation")]
+    public async Task<ActionResult<Location>> GetByIdAsync(int id){
+        var location = await _context.Locations.FindAsync(id);
+
+        if (location == null)
+            return NotFound();
+
+        return location;
+    }
+    [HttpPost]
+    public async Task<ActionResult<Location>> CreateAsync(Location location){
+        _context.Locations.Add(location);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtRoute("GetLocation",new { id = location.Id },location);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateAsync(int id, Location location)
+    {
+        if (id != location.Id)
+            return BadRequest();
+
+        _context.Entry(location).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var location = await _context.Locations.FindAsync(id);
+
+        if (location == null)
+            return NotFound();
+
+        _context.Locations.Remove(location);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
--- EcoMonitor.Api/Controllers/SensorController.cs
+++ EcoMonitor.Api/Controllers/SensorController.cs
@@ -8,16 +8,59 @@ namespace EcoMonitor.Api.Controllers;
 [Route("api/[controller]")]
 public class SensorController: ControllerBase{
     private readonly AppDbContext _context;
-    public SensorController(AppDbContext context)
-    {
+    public SensorController(AppDbContext context){
         _context = context;
     }
 
     [HttpGet]
-    public async Task<List<Sensor>> GetAllAsync()
-        {
-            return await _context.Sensors.ToListAsync();
+    public async Task<List<Sensor>> GetAllAsync(){
+        return await _context.Sensors.ToListAsync();
+    }
+
+    [HttpGet("{id}", Name = "GetSensorById")]
+    public async Task<ActionResult<Sensor>> GetByIdAsync(int id){
+        var sensor = await _context.Sensors.FindAsync(id);
+
+        if(sensor == null){
+            return NotFound();
         }
+    return sensor;
+    } 
+    [HttpPost]
+    public async Task<ActionResult<Sensor>> CreateAsync(Sensor sensor)
+    {
+        _context.Sensors.Add(sensor);
+
+        await _context.SaveChangesAsync();
+
+        return CreatedAtRoute("GetSensorById",new { id = sensor.Id },sensor);
+    }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateAsync(int id, Sensor sensor)
+    {
+        if (id != sensor.Id)
+            return BadRequest();
+
+        _context.Entry(sensor).State = EntityState.Modified;
+
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var sensor = await _context.Sensors.FindAsync(id);
+
+        if (sensor == null)
+            return NotFound();
+
+        _context.Sensors.Remove(sensor);
+
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
     
 }
 
