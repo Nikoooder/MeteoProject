ed087b5da25f36669f12f81db32ee379ff2d3660
Tejin <zadrotvbs@gmail.com>
2026-08-03 06:43:08 +0600

    testing excel import and export requests

--- EcoMonitor.Api/Controllers/LocationController.cs
+++ EcoMonitor.Api/Controllers/LocationController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 using EcoMonitor.Api.DTO;
+using ClosedXML.Excel;
 namespace EcoMonitor.Api.Controllers;
 [Authorize]
 [ApiController]
@@ -146,5 +147,66 @@ public class LocationController:ControllerBase{
         await _context.SaveChangesAsync();
 
         return NoContent();
+    }
+
+    [HttpGet("export")]
+    public async Task<IActionResult> ExportAsync()
+    {
+        var locations = await _context.Locations.Where(l => l.UserId == CurrentUserId).ToListAsync();
+
+        using var workbook = new XLWorkbook();
+
+        var worksheet = workbook.Worksheets.Add("Locations");
+
+        worksheet.Cell(1, 1).Value = "Name";
+        worksheet.Cell(1, 2).Value = "Latitude";
+        worksheet.Cell(1, 3).Value = "Longitude";
+
+        int row = 2;
+
+        foreach (var location in locations)
+        {
+            worksheet.Cell(row, 1).Value = location.Name;
+            worksheet.Cell(row, 2).Value = location.Latitude;
+            worksheet.Cell(row, 3).Value = location.Longitude;
+
+            row++;
+        }
+
+        using var stream = new MemoryStream();
+
+        workbook.SaveAs(stream);
+
+        return File(stream.ToArray(),"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","Locations.xlsx");
+    }
+    [HttpPost("import")]
+    public async Task<IActionResult> ImportAsync(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+            return BadRequest("Файл не выбран.");
+
+        using var stream = file.OpenReadStream();
+        using var workbook = new XLWorkbook(stream);
+
+        var worksheet = workbook.Worksheet(1);
+
+        var rows = worksheet.RowsUsed().Skip(1);
+
+        foreach (var row in rows)
+        {
+            var location = new Location
+            {
+                Name = row.Cell(1).GetString(),
+                Latitude = row.Cell(2).GetDouble(),
+                Longitude = row.Cell(3).GetDouble(),
+                UserId = CurrentUserId
+            };
+
+            _context.Locations.Add(location);
+        }
+
+        await _context.SaveChangesAsync();
+
+        return Ok("Импорт завершен.");
     }
 }
